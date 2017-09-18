ba7b3611eb23f5f76b6153b8d49f521880d14082
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-18 21:02:17 +0200

    Add Sneaky-throws examples (#3)

---
+++ sneaky-throws-lambda/src/main/java/com/pivovarit/sneaky/ThrowingFunction.java
@@ -0,0 +1,22 @@
+package com.pivovarit.sneaky;
+
+import java.util.function.Function;
+
+public interface ThrowingFunction<T, R> {
+    R apply(T t) throws Exception;
+
+    @SuppressWarnings("unchecked")
+    static <T extends Exception, R> R sneakyThrow(Exception t) throws T {
+        throw (T) t; // ( ͡° ͜ʖ ͡°)
+    }
+
+    static <T, R> Function<T, R> unchecked(ThrowingFunction<T, R> f) {
+        return t -> {
+            try {
+                return f.apply(t);
+            } catch (Exception ex) {
+                return ThrowingFunction.sneakyThrow(ex);
+            }
+        };
+    }
+}
