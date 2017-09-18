34df5780217d058fa02489155391bb77e499be75
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-18 20:17:44 +0200

    Add Sneaky-throws examples

---
+++ sneaky-throws-lambda/src/test/java/com/pivovarit/sneaky/ThrowingFunctionTest.java
@@ -0,0 +1,33 @@
+package com.pivovarit.sneaky;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.pivovarit.sneaky.ThrowingFunction.sneakyThrow;
+import static com.pivovarit.sneaky.ThrowingFunction.unchecked;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ThrowingFunctionTest {
+
+    @Test
+    void example_1() throws Exception {
+        assertThrows(IOException.class, () -> {
+            sneakyThrow(new IOException());
+        });
+    }
+
+    @Test
+    void example_2() throws Exception {
+        assertThrows(IOException.class, () -> {
+            Optional.of(42)
+              //.map(ThrowingFunctionTest::throwException) // Unhandled exception: java.io.IOException
+              .map(unchecked(ThrowingFunctionTest::throwException));
+        });
+    }
+
+    private static String throwException(Integer i) throws IOException {
+        throw new IOException("whoopsie.");
+    }
+}
