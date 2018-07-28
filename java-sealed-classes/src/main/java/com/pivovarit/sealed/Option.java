0e6d37f62b9413a3236eafca7d7679b1e9a10614
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-07-28 20:33:08 +0200

    Expand Option

--- java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
+++ java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
@@ -1,8 +1,10 @@
 package com.pivovarit.sealed;
 
-import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public abstract class Option<T> {
+
+    abstract T getOrElse(Supplier<T> other);
 
     private Option() {
     }
@@ -14,24 +16,17 @@ public abstract class Option<T> {
         public Some(T value) {
             this.value = value;
         }
+
+        @Override
+        T getOrElse(Supplier<T> other) {
+            return value;
+        }
     }
 
     public final static class None<T> extends Option<T> {
-    }
-
-    public final static class Match<T> {
-        private final Consumer<Some<T>> someConsumer;
-        private final Consumer<None<T>> noneConsumer;
-
-        private Match(Consumer<Some<T>> someConsumer, Consumer<None<T>> noneConsumer) {
-            this.someConsumer = someConsumer;
-            this.noneConsumer = noneConsumer;
-        }
-
-        public static <T> Match<T> of(
-          Consumer<Some<T>> someConsumer,
-          Consumer<None<T>> noneConsumer) {
-            return new Match<>(someConsumer, noneConsumer);
+        @Override
+        T getOrElse(Supplier<T> other) {
+            return other.get();
         }
     }
 }
