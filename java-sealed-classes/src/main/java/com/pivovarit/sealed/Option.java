c44b37595f9f28961e43170e3481fcb8737b100b
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-07-28 14:35:26 +0200

    temp

--- java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
+++ java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
@@ -1,5 +1,7 @@
 package com.pivovarit.sealed;
 
+import java.util.function.Consumer;
+
 public abstract class Option<T> {
 
     private Option() {
@@ -15,6 +17,22 @@ public abstract class Option<T> {
     }
 
     public final static class None<T> extends Option<T> {
+    }
+
+    public final static class Match<T> {
+        private final Consumer<Some<T>> someConsumer;
+        private final Consumer<None<T>> noneConsumer;
+
+        private Match(Consumer<Some<T>> someConsumer, Consumer<None<T>> noneConsumer) {
+            this.someConsumer = someConsumer;
+            this.noneConsumer = noneConsumer;
+        }
+
+        public static <T> Match<T> of(
+          Consumer<Some<T>> someConsumer,
+          Consumer<None<T>> noneConsumer) {
+            return new Match<>(someConsumer, noneConsumer);
+        }
     }
 }
 
