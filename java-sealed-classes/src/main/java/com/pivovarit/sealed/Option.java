cc3263e8997f3cc290772484bd041dcbda5c46ed
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-07-28 11:48:13 +0200

    Refactor examples

--- java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
+++ java-sealed-classes/src/main/java/com/pivovarit/sealed/Option.java
@@ -1,6 +1,22 @@
 package com.pivovarit.sealed;
 
-public class Option<T> {
+public abstract class Option<T> {
+
+    private Option() {
+    }
+
+    public final static class Some<T> extends Option<T> {
+
+        private final T value;
+
+        public Some(T value) {
+            this.value = value;
+        }
+    }
+
+    public final static class None<T> extends Option<T> {
+    }
 }
+
 
 
