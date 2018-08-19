6953ad2eb65ea2db31815c71867cbab7611693ac
pivovarit <gpiwowarek@gmail.com>
2018-08-19 15:27:26 +0300

    temp

--- java-nonuniform-random/src/main/java/com/pivovarit/random/WeightedRandom.java
+++ java-nonuniform-random/src/main/java/com/pivovarit/random/WeightedRandom.java
@@ -1,17 +1,20 @@
 package com.pivovarit.random;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
 
 public class WeightedRandom {
 
     public static <T extends Weightable> Optional<T> draw(Set<T> items) {
         return items.stream()
-          .min(comparing(e -> -Math.log(1 - ThreadLocalRandom.current().nextDouble()) / e.getWeight()));
+          .min(comparing(e -> -Math.log(ThreadLocalRandom.current().nextDouble()) / e.getWeight()));
     }
 
     public interface Weightable {
@@ -19,16 +22,54 @@ public class WeightedRandom {
     }
 
     public static void main(String[] args) {
-        Weightable i1 = () -> 1;
-        var items = Set.of(i1, () -> 2);
+        var items = Set.of(
+          new Item("i1", 1),
+          new Item("i2", 1),
+          new Item("i3", 1),
+          new Item("i4", 1)
+        );
 
-        int draws = 100000000;
-        var count = Stream.generate(() -> draw(items))
-          .limit(draws)
-          .filter(o -> o.orElse(null) == i1)
-          .count();
+        var results = Stream.generate(() -> draw(items))
+          .limit(10000000)
+          .flatMap(Optional::stream)
+          .collect(Collectors.groupingBy(i -> i.name, counting()));
 
-        System.out.println("Picked i1 " + count + " times out of " + draws);
+        System.out.println(results);
+    }
+
+    private static class Item implements Weightable {
+
+        private final String name;
+        private final int weight;
+
+        public Item(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        @Override
+        public int getWeight() {
+            return weight;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Item item = (Item) o;
+            return weight == item.weight &&
+              Objects.equals(name, item.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, weight);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("name='%s'}", name);
+        }
     }
 }
 
