82e9e8d53baf7f7adc53dfcb398fa5424bf86bdc
pivovarit <gpiwowarek@gmail.com>
2018-08-25 22:15:38 +0300

    Add a few examples

---
+++ java-advanced-groupingby/src/test/java/com/pivovarit/grouping/Scenario1Test.java
@@ -0,0 +1,69 @@
+package com.pivovarit.grouping;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.filtering;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+
+class Scenario1Test {
+
+    private List<String> strings = List.of("a", "bb", "cc", "ddd");
+
+    @Test
+    void standardGrouping() {
+        Map<Integer, List<String>> result = strings.stream()
+          .collect(groupingBy(String::length));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customMapImplementation() {
+        TreeMap<Integer, List<String>> result = strings.stream()
+          .collect(groupingBy(String::length, TreeMap::new, toList()));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregateImplementation() {
+        Map<Integer, TreeSet<String>> result = strings.stream()
+          .collect(groupingBy(String::length, toCollection(TreeSet::new)));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregation_counting() {
+        Map<Integer, Long> result = strings.stream()
+          .collect(groupingBy(String::length, counting()));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregation_filtering() {
+        Map<Integer, List<List<Character>>> result = strings.stream()
+          .map(toStringList())
+          .collect(groupingBy(List::size));
+
+        System.out.println(result);
+    }
+
+    private static Function<String, List<Character>> toStringList() {
+        return s -> s.chars().mapToObj(c -> (char) c).collect(toList());
+    }
+}
+
+
+
