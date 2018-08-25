69ea3f46df078e65eeec0bdd6fb4d73f90d660f8
pivovarit <gpiwowarek@gmail.com>
2018-08-25 23:22:35 +0300

    Add reducing/flatmapping examples

--- java-advanced-groupingby/src/test/java/com/pivovarit/grouping/Scenario1Test.java
+++ java-advanced-groupingby/src/test/java/com/pivovarit/grouping/Scenario1Test.java
@@ -2,18 +2,22 @@ package com.pivovarit.grouping;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.averagingInt;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.filtering;
+import static java.util.stream.Collectors.flatMapping;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.reducing;
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
 
@@ -73,6 +77,33 @@ class Scenario1Test {
     void customAggregation_averaging() {
         Map<Integer, Double> result = strings.stream()
           .collect(groupingBy(String::length, averagingInt(String::hashCode)));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregation_flatmapping() {
+        var result = strings.stream()
+          .map(toStringList())
+          .collect(groupingBy(List::size, flatMapping(Collection::stream, Collectors.toList())));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregation_reducing() {
+        var result = strings.stream()
+          .map(toStringList())
+          .collect(groupingBy(List::size, reducing(List.of(), (l1, l2) -> Stream.concat(l1.stream(), l2.stream()).collect(Collectors.toList()))));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void customAggregation_reducing_optional() {
+        var result = strings.stream()
+          .map(toStringList())
+          .collect(groupingBy(List::size, reducing((l1, l2) -> Stream.concat(l1.stream(), l2.stream()).collect(Collectors.toList()))));
 
         System.out.println(result);
     }
