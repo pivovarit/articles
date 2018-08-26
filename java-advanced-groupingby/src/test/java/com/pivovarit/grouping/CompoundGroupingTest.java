3b5bc7f81474f0e9edc950bc48338058a75e142a
pivovarit <gpiwowarek@gmail.com>
2018-08-26 23:07:32 +0300

    Add CompoundGroupingTest

--- java-advanced-groupingby/src/test/java/com/pivovarit/grouping/CompoundGroupingTest.java
+++ java-advanced-groupingby/src/test/java/com/pivovarit/grouping/CompoundGroupingTest.java
@@ -1,10 +1,55 @@
 package com.pivovarit.grouping;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.filtering;
+import static java.util.stream.Collectors.flatMapping;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.reducing;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
 
 class CompoundGroupingTest {
     private List<String> strings = List.of("a", "bb", "cc", "ddd");
 
+    @Test
+    void example_1() {
+        Map<Integer, TreeSet<String>> result = strings.stream()
+          .collect(
+            groupingBy(String::length,
+              mapping(String::toUpperCase,
+                filtering(s -> s.length() > 1,
+                  toCollection(TreeSet::new)))));
+
+        System.out.println(result);
+    }
+
+    @Test
+    void example_2() {
+        Map<Integer, String> result = strings.stream()
+          .collect(
+            groupingBy(String::length,
+              mapping(toStringList(),
+                flatMapping(s -> s.stream().distinct(),
+                  filtering(s -> s.length() > 0,
+                    mapping(String::toUpperCase,
+                      reducing("", (s, s2) -> s + s2)))))));
+
+        System.out.println(result);
+    }
+
+    private static Function<String, List<String>> toStringList() {
+        return s -> s.chars()
+          .mapToObj(c -> (char) c)
+          .map(Object::toString)
+          .collect(toList());
+    }
 }
 
 
