b037a3ffe5d4d24fe0e6a7269ef4e71f98c64ba8
pivovarit <gpiwowarek@gmail.com>
2018-09-18 19:34:10 +0200

    Add java-11-string-api-updates examples

---
+++ java-11-string-api-updates/src/test/java/com/pivovarit/string/StringApiUpdatesTest.java
@@ -0,0 +1,52 @@
+package com.pivovarit.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringApiUpdatesTest {
+
+    @Test
+    void shouldRepeat() {
+        var string = "foo bar ";
+
+        var result = string.repeat(2);
+
+        assertThat(result).isEqualTo(string + string);
+    }
+
+    @Test
+    void shouldRepeatEmpty() {
+        var string = "";
+
+        var result = string.repeat(Integer.MAX_VALUE);
+
+        assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    void shouldRepeatZeroTimes() {
+        var string = "foo";
+
+        var result = string.repeat(0);
+
+        assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    void shouldCheckIfBlank() {
+        assertThat(" ".isBlank()).isTrue();
+    }
+
+    @Test
+    void shouldStrip() {
+        assertThat(" ".strip()).isEmpty();
+        assertThat("  foo  ".stripLeading()).isEqualTo("foo  ");
+        assertThat("  foo  ".stripTrailing()).isEqualTo("  foo");
+    }
+
+    @Test
+    void shouldStreamLines() {
+        "foo\nbar".lines().forEach(System.out::println);
+    }
+}
