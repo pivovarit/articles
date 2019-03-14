c123b5def99da00b92a4a470c20516747efca712
pivovarit <gpiwowarek@gmail.com>
2019-03-14 14:01:36 +0100

    Fix ImprovedRandomSpliterator

--- java-random-stream/src/main/java/com/pivovarit/stream/ImprovedRandomSpliterator.java
+++ java-random-stream/src/main/java/com/pivovarit/stream/ImprovedRandomSpliterator.java
@@ -23,13 +23,18 @@ class ImprovedRandomSpliterator<T> implements Spliterator<T> {
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
-        int nextIdx = random.nextInt(size);
-        int lastIdx = size - 1;
+        if (size > 0) {
+            int nextIdx = random.nextInt(size);
+            int lastIdx = size - 1;
 
-        action.accept(source[nextIdx]);
-        source[nextIdx] = source[lastIdx];
-        source[lastIdx] = null; // let object be GCed
-        return --size > 0;
+            action.accept(source[nextIdx]);
+            source[nextIdx] = source[lastIdx];
+            source[lastIdx] = null; // let object be GCed
+            size--;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
