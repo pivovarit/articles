5e2059b5116ec21e9c15e1c3388ff3d7ca9a410c
pivovarit <gpiwowarek@gmail.com>
2019-03-24 19:25:34 +0100

    Add initial

---
+++ java-completable-future-all-of/src/test/java/com/pivovarit/timeouts/CompletableFutureAllOfTest.java
@@ -0,0 +1,18 @@
+package com.pivovarit.timeouts;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CompletableFutureAllOfTest {
+
+    @Test
+    void allOf() {
+    }
+
+}
