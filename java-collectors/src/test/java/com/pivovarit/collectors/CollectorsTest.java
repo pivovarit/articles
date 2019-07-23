087b0888ef6ba860f4ef473113c16c05727287ba
pivovarit <gpiwowarek@gmail.com>
2019-07-23 18:00:56 +0300

    Add collectingAndThen

--- java-collectors/src/test/java/com/pivovarit/collectors/CollectorsTest.java
+++ java-collectors/src/test/java/com/pivovarit/collectors/CollectorsTest.java
@@ -3,22 +3,32 @@ package com.pivovarit.collectors;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static java.util.stream.Collectors.toUnmodifiableMap;
+import static java.util.stream.Collectors.toUnmodifiableSet;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CollectorsTest {
 
     @Test
-    void toList() {
+    void E1_toList() {
         List<Integer> list = List.of(1, 2, 3);
 
         List<Integer> result = list.stream()
-          .collect(Collectors.toList());
+          .collect(toList());
 
         assertThat(result)
           .hasSize(3)
@@ -26,11 +36,11 @@ class CollectorsTest {
     }
 
     @Test
-    void toUnmodifiableList() {
+    void E2_toUnmodifiableList() {
         List<Integer> list = List.of(1, 2, 3);
 
         List<Integer> result = list.stream()
-          .collect(Collectors.toUnmodifiableList());
+          .collect(toUnmodifiableList());
 
         assertThat(result)
           .hasSize(3)
@@ -41,11 +51,11 @@ class CollectorsTest {
     }
 
     @Test
-    void toSet() {
+    void E3_toSet() {
         List<Integer> list = List.of(1, 2, 3, 3);
 
         Set<Integer> result = list.stream()
-          .collect(Collectors.toSet());
+          .collect(toSet());
 
         assertThat(result)
           .hasSize(3)
@@ -53,11 +63,11 @@ class CollectorsTest {
     }
 
     @Test
-    void toUnmodifiableSet() {
+    void E4_toUnmodifiableSet() {
         List<Integer> list = List.of(1, 2, 3, 3);
 
         Set<Integer> result = list.stream()
-          .collect(Collectors.toUnmodifiableSet());
+          .collect(toUnmodifiableSet());
 
         assertThat(result)
           .hasSize(3)
@@ -68,11 +78,11 @@ class CollectorsTest {
     }
 
     @Test
-    void toCollection() {
+    void E5_toCollection() {
         List<Integer> list = List.of(1, 2, 3);
 
         Collection<Integer> result = list.stream()
-          .collect(Collectors.toCollection(LinkedList::new));
+          .collect(toCollection(LinkedList::new));
 
         assertThat(result)
           .isInstanceOf(LinkedList.class)
@@ -80,5 +90,34 @@ class CollectorsTest {
           .containsOnly(1, 2, 3);
     }
 
+
+    @Test
+    void E6_toMap() {
+        List<String> list = List.of("one", "two", "three");
+
+        Map<String, Integer> result = list.stream()
+          .collect(toMap(e -> e, e -> e.length()));
+
+        assertThat(result)
+          .hasSize(3)
+          .containsEntry("one", 3)
+          .containsEntry("two", 3)
+          .containsEntry("three", 5);
+    }
+
+    @Test
+    void E7_collectingAndThen() {
+        List<String> list = List.of("one", "two", "three");
+
+        List<String> result = list.stream()
+          .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+
+        assertThat(result)
+          .hasSize(3)
+          .containsExactly("one", "two", "three");
+
+        assertThatThrownBy(() -> result.add(""))
+          .isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
 
 }
