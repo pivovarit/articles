8eb5844495fb2fb9d1cf4978ddeaea77348d090f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-25 11:32:07 +0100

    init

--- java-parallel-collection-processing/src/test/java/com/pivovarit/parallel/ParallelCollectorsTest.java
+++ java-parallel-collection-processing/src/test/java/com/pivovarit/parallel/ParallelCollectorsTest.java
@@ -4,14 +4,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.pivovarit.collectors.ParallelCollectors.parallel;
-import static com.pivovarit.collectors.ParallelCollectors.parallelToStream;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,48 +28,21 @@ class ParallelCollectorsTest {
 
     @Test
     void example_parallel_ordered() throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-
-        List<Integer> results = integers.stream()
-          .collect(parallel(Utils::process, toList(), executor, 10))
-          .join();
-
-        assertThat(results)
-          .containsExactlyElementsOf(integers);
+        // TODO
     }
 
     @Test
     void example_parallel_ordered_max_parallelism() throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-
-        List<Integer> results = integers.stream()
-          .collect(parallel(Utils::process, toList(), executor, 10))
-          .join();
-
-        assertThat(results)
-          .containsExactlyElementsOf(integers);
+        // TODO
     }
 
     @Test
     void example_parallel_ordered_async() throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-
-        CompletableFuture<List<Integer>> results = integers.stream()
-          .collect(parallel(Utils::process, toList(), executor, 10));
-
-        assertThat(results.join())
-          .containsExactlyElementsOf(integers);
+        // TODO
     }
 
     @Test
     void example_parallel_completion_order() throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-
-        List<Integer> results = integers.stream()
-          .collect(parallelToStream(Utils::process, executor, 10))
-          .collect(toList());
-
-        assertThat(results)
-          .containsExactlyInAnyOrderElementsOf(integers);
+        // TODO
     }
 }
