0b8755a538684a3945a9fc098cbedef41668b93d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-03-01 09:05:56 +0100

    Thread#onSpinWait fallback

---
+++ java-jdk8-on-spin-wait/src/main/java/com/pivovarit/thread/OnSpinWaitJDK8Adapter.java
@@ -0,0 +1,31 @@
+package com.pivovarit.thread;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+
+import static java.lang.invoke.MethodType.methodType;
+
+class OnSpinWaitJDK8Adapter {
+    private static final MethodHandle ON_SPIN_WAIT_HANDLE = resolve();
+
+    private OnSpinWaitJDK8Adapter() {
+    }
+
+    private static MethodHandle resolve() {
+        try {
+            return MethodHandles.lookup().findStatic(Thread.class, "onSpinWait", methodType(void.class));
+        } catch (Exception ignore) {
+        }
+
+        return null;
+    }
+
+    static void onSpinWait() {
+        if (ON_SPIN_WAIT_HANDLE != null) {
+            try {
+                ON_SPIN_WAIT_HANDLE.invokeExact();
+            } catch (Throwable ignore) {
+            }
+        }
+    }
+}
