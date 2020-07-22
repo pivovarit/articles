2844366bd9f4acbb74864142b1f2b1c07c9d3461
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-07-22 05:40:15 +0200

    Update Quarkus code samples

--- quarkus/quarkus-hello/src/main/java/org/acme/ExampleResource.java
+++ quarkus/quarkus-hello/src/main/java/org/acme/ExampleResource.java
@@ -1,12 +1,12 @@
-package com.pivovarit.quarkus;
+package org.acme;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/foo")
-public class HelloResource {
+@Path("/hello")
+public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
--- quarkus/quarkus-hello/src/test/java/org/acme/ExampleResourceTest.java
+++ quarkus/quarkus-hello/src/test/java/org/acme/ExampleResourceTest.java
@@ -1,4 +1,4 @@
-package com.pivovarit.quarkus;
+package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -7,12 +7,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class HelloResourceTest {
+public class ExampleResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/foo")
+          .when().get("/hello")
           .then()
              .statusCode(200)
              .body(is("hello"));
