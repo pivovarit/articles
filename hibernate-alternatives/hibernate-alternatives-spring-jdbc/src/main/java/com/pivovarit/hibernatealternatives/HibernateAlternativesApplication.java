8710aee1764ee8c45e2c9c02ac51600ff89abd32
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-08-15 08:15:18 +0200

    Add empty project

---
+++ hibernate-alternatives/hibernate-alternatives-spring-jdbc/src/main/java/com/pivovarit/hibernatealternatives/HibernateAlternativesApplication.java
@@ -0,0 +1,13 @@
+package com.pivovarit.hibernatealternatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAlternativesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAlternativesApplication.class, args);
+	}
+
+}
