107b98f72e3f3d3e6e94514c000fd5accc95b475
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2021-08-26 08:45:48 +0200

    Update maven-enforcer-plugin

--- quarkus/quarkus-hello/src/main/java/com/pivovarit/HelloResource.java
+++ quarkus/quarkus-hello/src/main/java/com/pivovarit/HelloResource.java
@@ -6,11 +6,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class GreetingResource {
+public class HelloResource   {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "hello";
     }
 }
