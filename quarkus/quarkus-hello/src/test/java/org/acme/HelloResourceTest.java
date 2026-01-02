7c4755041c18c45f163a619f3908561fc7c4c6d3
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-01-02 20:51:00 +0100

    Upgrade Quarkus

--- quarkus/quarkus-hello/src/test/java/org/acme/HelloResourceTest.java
+++ quarkus/quarkus-hello/src/test/java/org/acme/HelloResourceTest.java
@@ -1,4 +1,4 @@
-package com.pivovarit;
+package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +7,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
-
+class HelloResourceTest {
     @Test
-    public void testHelloEndpoint() {
+    void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("Hello from Quarkus REST"));
     }
 
 }
